76aa28f7564331e5d7f59f58e8b956313a3904e3
AMAR PRASAD <amarprasad282005@gmail.com>
2023-06-22 16:51:07 +0530

    implementation for diff

--- contact.java
+++ contact.java
@@ -1,3 +1,10 @@
 public class contact{
+    public static void main(String [] args){
+        int a = 6;
+        int b = 4;
+        int c = a+b;
+
+        System.out.println("The sum is"+c);
+    }
     
 }
